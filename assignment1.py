869ab74d1b9d920b450b7e99f37d65b73a49b7fb
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-08-31 22:21:21 +0200

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+1. a
+print('Knowledge is power!')
+1. b
+print('Knowledge\nis\npower!')
+print('Knowledge')
+x = 'is'
+y = 'Power!'
+x.center(3)
+print(x.center(7))
+print(y.center(10))
+1. c
+2.
+x = input('Write a line of text: ')
+print('Quote: ' + f'"{x}"')
