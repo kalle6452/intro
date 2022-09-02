7e9450a7eeea318b97d5c16ece4b140aab959f6a
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-09-02 12:11:04 +0200

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -12,3 +12,15 @@ print(y.center(10))
 2.
 x = input('Write a line of text: ')
 print('Quote: ' + f'"{x}"')
+3.
+F = float(input('Skriv in temperatur i fahrenheit '))
+x = 9/5
+C = (F - 32)/x
+print(C)
+5.
+import math
+r = int(input('Skriv in radien'))
+r = r**3
+x = 4/3
+v = x*math.pi*r
+print(round(v,1))
