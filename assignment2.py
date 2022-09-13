fce093d279cd61a07105b5243d65c0e12a79c026
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-09-13 16:49:27 +0200

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,5 +1,16 @@
+1.
 summa = 0
 for i in range(1,101):
     if i%2==0:
         summa += i
 print(summa)
+2.
+count = 0
+sum = 0
+
+for i in range(0,100):
+    sum = sum + count
+    count = count + 1
+
+print(sum)
+3.
