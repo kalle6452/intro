391c693d3bf4c81d82013cf15a762095a55e765b
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-09-15 00:01:33 +0200

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -73,3 +73,25 @@ for i in range(1,7):
 svar = (max(lista) - min(lista))/max(lista)
 svar *= 100
 print(f'om man kastar tärningen {rolls} är skillnaden {svar}%')
+6.
+import random
+störst = 0
+minst = 0
+alla = 0
+n = int(input('Skriv in hur många siffror som ska skrivas ut '))
+print('Genererade tal:',end=' ')
+for i in range(n):
+    tal = random.randint(1,100)
+    print(tal, end=' ')
+    if i == 0:
+        störst += tal
+        alla += tal
+        minst += tal
+    else:
+        alla += tal
+        if tal > störst:
+            störst = tal
+        elif tal < minst:
+            minst = tal
+print()
+print(f'störst, minst, genomsnitt: {störst}, {minst}, {alla/n}')
