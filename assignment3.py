70bacbd51d92a66ba107d9c0144facfdb89709b5
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-09-28 14:42:15 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,5 +1,11 @@
 import os
 path = os.getcwd() 
 print("Current dir:", path)
-path1 =  os.startfile(path)
+path1 = os.startfile(path)
 print(path1)
+
+
+entries = os.scandir(path)
+with os.scandir(path) as entries:
+    for entry in entries:
+        print(entry.name)
