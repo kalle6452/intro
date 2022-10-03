1465dcfd4be05abf8665209a3c80192cb86edeb7
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-10-04 01:13:28 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,4 @@
+1.
 import os
 from pathlib import Path
 path = os.getcwd()
@@ -8,3 +9,28 @@ def count_files(path):
     return f'antal filer: {len([entry for entry in os.listdir(path) if os.path.isfile(os.path.join(path, entry))])}'
 print(count_directories(path))
 print(count_files(path))
+2.
+import os
+path = os.getcwd()
+print(path)
+text = '''1. List directories
+2. Change directory
+3. List files
+4. Quit
+'''
+val = int(input(text))
+while val != 4:
+    if val == 1:
+        def list_dir(path):
+            return next(os.walk('.'))[1]
+        print(list_dir(path))
+    elif val == 3:
+        def list_files(path):
+            return next(os.walk('.'))[2]
+        print(list_files(path))
+    elif val == 2:
+        x = input('katalog ')
+        os.chdir(x)
+        path = os.getcwd()
+        print(path)
+    val = int(input('s '))
