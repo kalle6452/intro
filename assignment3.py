6d0a79ee20c91eccbe78467303bf421822b2bff8
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-10-08 17:02:56 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -46,3 +46,15 @@ def different(x):
     y = sorted(y)
     return list(y)
 print(different(lst))
+12.
+import random
+lst = []
+for i in range(100):
+    y = random.randint(1,10)
+    lst.append(y)
+dic = {1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0,10:0,}
+for i in dic:
+    for y in lst:
+        if y == i:
+            dic[i] += 1
+print(dic)
