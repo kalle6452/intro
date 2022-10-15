b90e6b0fe9ab4ad92365297db7fe600ea4162da8
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-10-16 00:31:42 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -202,3 +202,78 @@ print(lista)
 for i in lista:
     print(i)
 
+16.
+from dataclasses import dataclass
+from typing import Any
+
+# A head-and-tail implementation of a deque using data classes
+
+
+# Each node is an instance of class Node
+@dataclass
+class Node:
+    value: int = None
+    nxt: Any = None  # Any since Node not properly defined at this point
+
+
+@dataclass
+class Deque:
+    head: Node = None      # First node in queue
+    tail: Node = None      # Last node in queue
+    size: int = 0
+
+    # Add element n as last entry in deque
+    def add_last(self, n):
+        new = Node(n, None) # Node to be added
+        if self.head is None: # An empty list
+            self.head = new
+        else: # Non-empty ==> Find last node
+            node = self.head # and attach new node as last
+            while node.nxt is not None:
+                node = node.nxt # Move to next node
+            node.nxt = new
+        self.size += 1
+
+    # Returns a string representation of the current deque content
+    def to_string(self):
+        s = "{ "
+        node = self.head
+        while node is not None:
+            s += str(node.value) + " "
+            node = node.nxt
+        s += "}"
+        return s
+
+    # Add element n as first entry in deque
+    def add_first(self, n):
+        new = Node(n, None) # Node to be added
+        if self.head is None: # An empty list
+            self.head = new
+        else: # Non-empty ==> Find last node
+            node = self.head # and attach new node as last
+            while node.nxt is not None:
+                node = node.nxt # Move to next node
+            node.nxt = new
+        self.size += 1
+
+    # Returns (without removing) the last entry in the deque.
+    # Gives error message and returns None when accessing empty deque.
+    def get_last(self):
+        pass
+
+    # Returns (without removing) the first entry in the deque
+    # Gives error message and returns None when accessing empty deque.
+    def get_first(self):
+        pass
+
+    # Removes and returns the first entry in the deque.
+    # Gives error message and returns None when accessing empty deque.
+    # The case size = 1 requires speciall handling
+    def remove_first(self):
+        pass
+
+    # Removes and returns the last entry in the deque.
+    # Gives error message and returns None when accessing empty deque.
+    # The case size = 1 requires speciall handling
+    def remove_last(self):
+        pass
