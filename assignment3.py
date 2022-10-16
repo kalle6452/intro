d8d7e16e10c7c8f9d7997dab9bf636c19c7fbbae
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2022-10-16 20:43:35 +0200

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -4,10 +4,16 @@ import os
 from pathlib import Path
 path = os.getcwd()
 print(f'Current path: {Path.cwd()}')
+
+
 def count_directories(path):
     return f'antal kataloger: {len([entry for entry in os.listdir(path) if os.path.isdir(os.path.join(path, entry))])}'
+
+
 def count_files(path):
     return f'antal filer: {len([entry for entry in os.listdir(path) if os.path.isfile(os.path.join(path, entry))])}'
+
+
 print(count_directories(path))
 print(count_files(path))
 2.
